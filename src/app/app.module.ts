77a41650744408f7ca3b8034ad34b6c08a31adbc
David Molina <molina.angeldavid@gmail.com>
2023-04-30 17:42:13 -0300

    remove nav from header and add components routes to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { ProductoComponent } from './componentes/producto/producto.component'
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    ProductoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
