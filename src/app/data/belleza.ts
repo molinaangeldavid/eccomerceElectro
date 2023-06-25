9e5d7fb2836f92677f6d5417f2854e3e79c120bf
david molina <molina.angeldavid@gmail.com>
2023-06-24 23:08:04 -0300

    add data tec and beauty images

---
+++ src/app/data/belleza.ts
@@ -0,0 +1,73 @@
+export const belleza = [
+    {
+        codigo: 10601,
+        categoria: "belleza",
+        nombre: "Secador de pelo Atma SP8964N negro 125V/220V - 240V",
+        descripcion: "",
+        descripcionCorta: "Secador de pelo Atma color negro",
+        imagen: "../assets/secador-de-pelo-Atma-SP8964.webp",
+        marca: "Atma",
+        precio: 9.899,
+        velocidad: 2
+    },
+    {
+        codigo: 10602,
+        categoria: "belleza",
+        nombre: "Secador de pelo GA.MA Italy Mistral Ceramic",
+        descripcion: "",
+        descripcionCorta: "Secador de pelo GAMA con 1.7mts de cable",
+        imagen: "../assets/secador-de-pelo-GA-MA-Italy.webp",
+        marca: "Gama",
+        precio: 16.899,
+        velocidad: 2
+    },
+    {
+        codigo: 10603,
+        categoria: "belleza",
+        nombre: "Secador De Pelo Atma Sp8970p De 1800w 3 Velocidades",
+        descripcion: "",
+        descripcionCorta: "Secador de pelo Atma con 3 velocidades. accesorios de peine y mas.",
+        imagen: "../assets/secador-De-Pelo-Atma-Sp8970p-De-1800w.webp",
+        marca: "Atma",
+        precio: 10.999,
+        velocidad: 3
+    },
+    {
+        codigo: 10702,
+        categoria: "belleza",
+        nombre: "depiladora Inalambrica De Pinzas Gama Velvet",
+        descripcion: "",
+        descripcionCorta: "Depiladora Inalambrica Gama para ellas. Para tener una piel suave y sedosa por todo tu cuerpo",
+        imagen: "../assets/depiladora-Inalambrica-De-Pinzas-Gama-Velvet.webp",
+        marca: "Gama",
+        color:"rosa",
+        precio: 11.489,
+        velocidad: 2
+        
+    },
+    {
+        codigo: 10703,
+        categoria: "belleza",
+        nombre: "Depiladora Care By Gadnic Lady600 9w",
+        descripcion: "",
+        descripcionCorta: "Depiladora Inalambrica Gadnic. Con dos cabezales. Es lavable y recargable",
+        imagen: "../assets/depiladora-Care-By-Gadnic.webp",
+        marca: "Gadnic",
+        color:"verde",
+        precio: 8.999,
+        velocidad: 2
+    },
+    {
+        codigo: 10810,
+        categoria: "belleza",
+        nombre: "Afeitadora GA.MA Italy GSH 855 Master negra ",
+        descripcion: "",
+        descripcionCorta: "Afeitadora para ellos y ellas. Marca Gama Master. Funciona re piola",
+        imagen: "../assets/afeitadora-GAMA-Italy-GSH855-Master-negra.webp",
+        marca: "Gama",
+        color:"negro",
+        precio: 10.399,
+        velocidad: 2
+    },
+    
+]
