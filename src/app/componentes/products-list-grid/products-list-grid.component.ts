2b1580bdf7f357c072d2cbdc9d6c6ff866736b6c
david molina <molina.angeldavid@gmail.com>
2023-06-26 22:49:31 -0300

    add routing and more

--- src/app/componentes/products-list-grid/products-list-grid.component.ts
+++ src/app/componentes/products-list-grid/products-list-grid.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit,SimpleChanges } from '@angular/core';
+import { hogar } from 'src/app/data/hogar';
+import { tecnologia } from 'src/app/data/tecnologia';
+import { belleza } from 'src/app/data/belleza';
+import { taller } from 'src/app/data/taller';
 
 @Component({
   selector: 'app-products-list-grid',
   templateUrl: './products-list-grid.component.html',
   styleUrls: ['./products-list-grid.component.scss']
 })
-export class ProductsListGridComponent {
+export class ProductsListGridComponent implements OnInit, OnChanges{
+
+  products!: any[];
+
+  ngOnInit(){
+    let randomArray = Math.round(Math.random() * 4)
+    switch(randomArray){
+      case 1: 
+        this.products = hogar;
+        break;
+      case 2: 
+        this.products = tecnologia;
+        break;
+      case 3: 
+        this.products = belleza;
+        break;
+      default: 
+        this.products = taller;
+        break;
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.products)
+    console.log(changes)
+  }
+
+  categoryProduct!: string;
+
+  filterProducts(str: string): any[]{
+    let filterProducts: any = [];
+    let filter1 = hogar.filter(x => x.categoria === str);
+    let filter2 = tecnologia.filter(x => x.categoria === str)
+    let filter3 = belleza.filter(x => x.categoria === str)
+    let filter4 = taller.filter(x => x.categoria === str)
+    if(filter1.length != 0){
+      filterProducts = filter1;
+    }else if(filter2.length != 0){
+      filterProducts = filter2;
+    }else if(filter3.length != 0){
+      filterProducts = filter3
+    }else{
+      filterProducts = filter4
+    }
+    console.log(filterProducts)
+    return filterProducts;
+  }
 
 }
