cf030643a9175842af20ef9913907199ac823685
david molina <molina.angeldavid@gmail.com>
2023-06-28 14:34:53 -0300

    arreglo funcionamiento filtro por categoria, pero no anda la paginacion

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -19,54 +19,10 @@ export class HomeComponent implements OnInit,OnChanges{
   products!: any[];
 
   ngOnInit(){
-    let randomArray = Math.round(Math.random() * 4)
-    switch(randomArray){
-      case 1: 
-        this.products = hogar;
-        break;
-      case 2: 
-        this.products = tecnologia;
-        break;
-      case 3: 
-        this.products = belleza;
-        break;
-      default: 
-        this.products = taller;
-        break;
-    }
+    
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(this.products)
-    console.log(changes)
   }
-
-  categoryProduct!: string;
-
-  filterProducts(str: string): any[]{
-    let filterProducts: any = [];
-    let filter1 = hogar.filter(x => x.categoria === str);
-    let filter2 = tecnologia.filter(x => x.categoria === str)
-    let filter3 = belleza.filter(x => x.categoria === str)
-    let filter4 = taller.filter(x => x.categoria === str)
-    if(filter1.length != 0){
-      filterProducts = filter1;
-    }else if(filter2.length != 0){
-      filterProducts = filter2;
-    }else if(filter3.length != 0){
-      filterProducts = filter3
-    }else{
-      filterProducts = filter4
-    }
-    console.log(filterProducts)
-    return filterProducts;
-  }
-
-  filterBy(event:string){
-    this.products = this.filterProducts(event);
-    this.productsService.setProducts(this.products)
-  }
-
-  
 
 }
